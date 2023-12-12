02d3e146cf2f0eedb1fab1c17a961a734ea921a4
saustin-a <150204894+saustin-a@users.noreply.github.com>
2023-12-12 10:19:54 -0600

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -33,6 +33,7 @@ passport.deserializeUser(async (id, done) => {
  * Sign in using Email and Password.
  */
 passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  // edit a sensitive file
   User.findOne({ email: email.toLowerCase() })
     .then((user) => {
       if (!user) {
